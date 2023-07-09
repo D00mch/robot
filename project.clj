47054742a47f0b518583cc0c8a18a4ee1c08ce63
liverm0r <arturdumchev@yandex.ru>
2023-07-09 16:19:31 +0300

    Update versions

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject robot "0.3.1-SNAPSHOT"
+(defproject robot "0.3.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns robot.core})
