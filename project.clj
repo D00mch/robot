96839ed3883ae197564554a0866e2799fbd0d4fc
liverm0r <arturdumchev@yandex.ru>
2023-07-09 21:47:03 +0300

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject robot "0.3.2-SNAPSHOT"
+(defproject robot "0.3.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
