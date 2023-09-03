3f92bae14f00cef379d7d5a1813d4c3b54e9644d
dumch <arturdumchev@yandex.ru>
2023-09-03 23:40:48 +0300

    Increase version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject robot "0.3.4-SNAPSHOT"
+(defproject robot "0.3.5-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
